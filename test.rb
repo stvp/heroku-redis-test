a43acafafebcf5063eff582d7a6ca104f8604984
Tyson Tate <tyson@tysontate.com>
2015-01-14 16:37:56 -0800

    Try with and without HTTParty.

--- test.rb
+++ test.rb
@@ -2,7 +2,6 @@ puts "Starting up"
 
 require 'rubygems'
 require 'bundler/setup'
-require 'httparty'
 require 'json'
 require 'redis'
 
@@ -19,24 +18,46 @@ $api_accept = ENV["API_ACCEPT"]
 # Fetch servers
 #
 
-puts "Fetching #{$count} random servers"
+if rand > 0.5
+  require 'httparty'
 
-response = HTTParty.get( $api_url, {
-  query: { count: $count },
-  headers: {
-    "X-API-Token" => $api_token,
-    "Accept" => $api_accept
-  }
-})
-servers = JSON.parse( response.body )
+  start = Time.now
 
-puts "Got #{servers.size} servers"
+  response = HTTParty.get( $api_url, {
+    query: { count: $count },
+    headers: {
+      "X-API-Token" => $api_token,
+      "Accept" => $api_accept
+    }
+  })
+  $servers = JSON.parse( response.body )
+
+  puts "FETCHED\thttparty\t#{$servers.count} servers\t${Time.now-start}"
+else
+  require 'uri'
+  require 'net/http'
+
+  start = Time.now
+
+  uri = URI( $api_url+"?count=#{$count}" )
+  params = { count: $count }
+  uri.query = URI.encode_www_form( params )
+
+  req = Net::HTTP::Get.new( uri )
+  req["X-API-Token"] = $api_token
+  req["Accept"] = $api_accept
+  http = Net::HTTP.new( uri.host, uri.port )
+  http.use_ssl = uri.scheme == "https"
+  $servers = JSON.parse( http.request( req ).body )
+
+  puts "FETCHED\tnet/http\t#{$servers.count}\t#{Time.now-start}"
+end
 
 #
 # Test
 #
 
-servers.each do |server|
+$servers.each do |server|
   (1..4).each do |i|
     label = "#{server['host']}:#{server['port']}"
     begin
