11f0604c22c8ca7f5a05ef625def09a5adef9b90
Tyson Tate <tyson@tysontate.com>
2015-01-16 14:08:13 -0800

    Remove HTTParty.

--- test.rb
+++ test.rb
@@ -2,6 +2,8 @@ puts "Starting up"
 
 require 'rubygems'
 require 'bundler/setup'
+require 'uri'
+require 'net/http'
 require 'json'
 require 'redis'
 
@@ -18,40 +20,20 @@ $api_accept = ENV["API_ACCEPT"]
 # Fetch servers
 #
 
-if rand > 0.5
-  require 'httparty'
+start = Time.now
 
-  start = Time.now
+uri = URI( $api_url+"?count=#{$count}" )
+params = { count: $count }
+uri.query = URI.encode_www_form( params )
 
-  response = HTTParty.get( $api_url, {
-    query: { count: $count },
-    headers: {
-      "X-API-Token" => $api_token,
-      "Accept" => $api_accept
-    }
-  })
-  $servers = JSON.parse( response.body )
+req = Net::HTTP::Get.new( uri )
+req["X-API-Token"] = $api_token
+req["Accept"] = $api_accept
+http = Net::HTTP.new( uri.host, uri.port )
+http.use_ssl = uri.scheme == "https"
+$servers = JSON.parse( http.request( req ).body )
 
-  puts "FETCHED\thttparty\t#{$servers.count} servers\t#{Time.now-start}"
-else
-  require 'uri'
-  require 'net/http'
-
-  start = Time.now
-
-  uri = URI( $api_url+"?count=#{$count}" )
-  params = { count: $count }
-  uri.query = URI.encode_www_form( params )
-
-  req = Net::HTTP::Get.new( uri )
-  req["X-API-Token"] = $api_token
-  req["Accept"] = $api_accept
-  http = Net::HTTP.new( uri.host, uri.port )
-  http.use_ssl = uri.scheme == "https"
-  $servers = JSON.parse( http.request( req ).body )
-
-  puts "FETCHED\tnet/http\t#{$servers.count}\t#{Time.now-start}"
-end
+puts "FETCHED\t#{$servers.count}\t#{Time.now-start}"
 
 #
 # Test
